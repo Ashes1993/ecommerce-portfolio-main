4c79cf87fc6745b0ae2820f039cfbbf94a11f1fb
Ashes <ashkan.eddyking@gmail.com>
2024-05-21 14:06:46 +0330

    Made adjustments to the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,37 @@
-import React from 'react'
-import { FaCartArrowDown } from 'react-icons/fa'
-import { useGlobalContext } from '../context'
+import React from "react";
+import { FaCartArrowDown } from "react-icons/fa";
+import { useGlobalContext } from "../context";
+import laptopLogo from "../images/laptop-logo.jpg";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-  const { shopingCart, isCartOpen, setIsCartOpen } = useGlobalContext()
+  const { shopingCart, isCartOpen, setIsCartOpen } = useGlobalContext();
   return (
-    <div className='navbar'>
-      <h3 className='navbar-header'>Online Shop</h3>
-      <div className='shoping-cart' onClick={() => setIsCartOpen(!isCartOpen)}>
-        <FaCartArrowDown className='cart-icon' />
-        <span className='product-counter'>{shopingCart.length}</span>
+    <header className="navbar">
+      <a className="navbar-header" href="/">
+        <img className="logo-image" src={laptopLogo} alt="laptop logo" />
+      </a>
+      <ul className="navbar-buttons">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <Link to="laptops" smooth={true} duration={300}>
+            Laptops
+          </Link>
+        </li>
+        <li>
+          <Link to="contact-us" smooth={true} duration={300}>
+            Contact Us
+          </Link>
+        </li>
+      </ul>
+      <div className="shoping-cart" onClick={() => setIsCartOpen(!isCartOpen)}>
+        <FaCartArrowDown className="cart-icon" />
+        <span className="product-counter">{shopingCart.length}</span>
       </div>
-    </div>
-  )
-}
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
