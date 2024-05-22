6bb7a4080f4e30839ec9c5ba1e30708fb7d73181
Ashes <ashkan.eddyking@gmail.com>
2024-05-22 17:08:40 +0330

    Added a success message when pushing the button "Add to Cart"

--- src/components/ProductItems.js
+++ src/components/ProductItems.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGlobalContext } from "../context";
 import { productList } from "../data";
 import { Element } from "react-scroll";
 
-export const ProductItems = () => {
-  const { products, productError, shopingCart, setShopingCart } =
-    useGlobalContext();
-
+const ProductCard = ({ id, brand, model, price, imgUrl }) => {
+  const [isAdded, setIsAdded] = useState(false);
+  const { shopingCart, setShopingCart } = useGlobalContext();
   const addCart = (id) => {
     const itemAdded = productList.filter((product) => id === product.id);
-    let newShopingCart = [...shopingCart, ...itemAdded];
-    setShopingCart(newShopingCart);
-  };
+    const isProductDuplicate = shopingCart.some((item) => item.id === id);
 
+    if (!isProductDuplicate) {
+      let newShopingCart = [...shopingCart, ...itemAdded];
+      setShopingCart(newShopingCart);
+    } else {
+      console.log("item is duplicated!");
+    }
+
+    setIsAdded(true);
+    setTimeout(() => setIsAdded(false), 3000);
+    return;
+  };
+  return (
+    <article className="laptop-item" key={id}>
+      <img className="laptop-photo" src={imgUrl} alt="laptop" />
+      <div className="laptop-details">
+        <span className="laptop-brand">Brand: {brand}</span>
+        <span className="laptop-model">Model: {model}</span>
+        <p className="laptop-price">{price}$</p>
+        <button className="add-cart" onClick={() => addCart(id)}>
+          Add to Cart
+        </button>
+        {isAdded && (
+          <p className="add-cart-success">Item added successfully!</p>
+        )}
+      </div>
+    </article>
+  );
+};
+
+export const ProductItems = () => {
+  const { products, productError } = useGlobalContext();
   if (productError) {
     return (
       <div className="no-product">
@@ -24,24 +52,9 @@ export const ProductItems = () => {
   return (
     <Element name="laptops">
       <section className="products-items" id="laptops">
-        {products.map((product) => {
-          return (
-            <div className="laptop-item" key={product.id}>
-              <img className="laptop-photo" src={product.imgUrl} alt="laptop" />
-              <div className="laptop-details">
-                <span className="laptop-brand">Brand: {product.brand}</span>
-                <span className="laptop-model">Model: {product.model}</span>
-                <p className="laptop-price">{product.price}$</p>
-                <button
-                  className="add-cart"
-                  onClick={() => addCart(product.id)}
-                >
-                  Add to Cart
-                </button>
-              </div>
-            </div>
-          );
-        })}
+        {products.map((product, index) => (
+          <ProductCard key={index} {...product} />
+        ))}
       </section>
     </Element>
   );
